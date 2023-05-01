f3e9d2e788c9a1aacafc2dc0675820662564f7dd
Finn Feddersen <fife22ab@student.cbs.dk>
2023-05-01 20:37:50 +0200

    App layout change

--- src/app.py
+++ src/app.py
@@ -5,13 +5,12 @@ from dash import Dash, dcc, html
 from components.layout import create_layout
 from components.import_cdr import import_data
 
-def main() -> None:
-    data = import_data()
-    app = Dash(external_stylesheets=[BOOTSTRAP])
-    app.title = "Carbon Dioxide Removal - Certificate Market"
-    app.layout = create_layout(app, data)
-    app.run(port="8051")
-    server = app.server
+data = import_data()
+app = Dash(external_stylesheets=[BOOTSTRAP])
+app.title = "Carbon Dioxide Removal - Certificate Market"
+app.layout = create_layout(app, data)
+
+server = app.server
 
 if __name__ == "__main__":
-    main()
+    app.run_server(debug=True, port="8051")
